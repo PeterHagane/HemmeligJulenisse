e825f67af7233304914010d47f41f4348e1de2fb
Peter Hagane <peterhagane@yahoo.no>
2023-11-18 17:37:57 +0100

    add form

--- js/splash.js
+++ js/splash.js
@@ -4,14 +4,14 @@ function getYear() {
 }
 
 var splashHTML = `
-<div class="flexColumn"> 
+<div class="flexColumn splashIntro flexCenter"> 
     <img/>
-    <h4>
+    <h2 class="text-gr">
     Hemmelig Julefisk ${getYear()}
-    </h4>
+    </h2>
     <div class="flexRow">
     <ul id="splashmenu" class="splashmenu">
-    <li><a href="#about">About</a></li>
+    <li><a href="#about" onclick="renderSignUp()">Påmelding</a></li>
     </ul>
     </div>
 </div> 
@@ -22,9 +22,48 @@ var splashHTML = `
 
 var setSplash = function setSplash(){
     var splash = document.getElementById("splash");
-    splash.setAttribute("class", "flexColumn text-gr");
+    splash.setAttribute("class", "flexColumn flexCenter visible");
     splash.innerHTML = splashHTML;
 }
 setSplash();
+
+async function derenderSplash(){
+    
+    var splash = document.getElementById("splash");
+    splash.classList.add("hidden")
+    await delay(500);
+    splash.classList.remove("hidden")
+    splash.innerHTML = "";
+    splash.classList.add("visible")
+    return true
+};
+
+splashHTML = "<div>asd</div>"
+
+async function renderSignUp(){
+    await derenderSplash().then(()=>{
+        splashHTML = `
+        <div class="flexColumn splashIntro flexCenter"> 
+        <h3>Lag en ny event</h3>
+            <form>
+                <div class="uk-margin">
+                    <div class="uk-inline">
+                        <span class="uk-form-icon" uk-icon="icon: user"></span>
+                        <input class="uk-input" type="text" aria-label="Not clickable icon">
+                    </div>
+                </div>
+                
+                <div class="uk-margin">
+                    <div class="uk-inline">
+                        <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: lock"></span>
+                        <input class="uk-input" type="text" aria-label="Not clickable icon">
+                    </div>
+                </div>
+            </form>
+            </div>
+        `
+        setSplash()
+    })
+}
 
 //<h4>By</h4><h1>PETER HAGANE</h1></div><div class="flexRow">
